103f6f9a446b3ad238072636eb341c00a49710ac
konda ravilla <kondaravilla000@gmail.com>
2023-12-02 12:47:27 +0530

    content updated

--- src/portals/Modal.js
+++ src/portals/Modal.js
@@ -3,14 +3,15 @@ import './modal.css';
 import ReactDOM from 'react-dom';
 
 function Modal(props) {
-  return (
+  return ReactDOM.createPortal(
     <div className='modal-layer'>
       <div className='modal-content'>
         <h3>Modal content</h3>
         <p>This is modal click here to close modal</p>
         <button onClick={props.closeModal}>close modal</button>
       </div>
-    </div>
+    </div>,
+    document.getElementById('portal-div')
   );
 }
 
